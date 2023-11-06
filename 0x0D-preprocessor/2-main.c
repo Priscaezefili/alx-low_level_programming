5820693ac14994c97febfbbbd0acea13ddb6fa6e
priscaezefili <priscaezefili@gmail.com>
2023-11-06 13:38:12 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints name of file
  *
- * Return: Always 0 when it is successful
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
